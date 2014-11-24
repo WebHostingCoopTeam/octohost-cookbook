1f1019842f8d33e92898b3e616554ddbc83a22b0
Josh Cox <josh@webhosting.coop>
2014-11-24 17:25:10 -0600

    movein.sh, headless, format raw

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-/usr/bin/time -v rake build_vagrant_without_tests
+#/usr/bin/time -v rake build_vagrant_without_tests
+/usr/bin/time -v rake build_qemu
 # beeper
 #aplay /usr/share/sounds/alsa/Front_Center.wav
 ogg123 /usr/share/sounds/KDE-Im-Irc-Event.ogg
