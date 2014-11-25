16a1e811d91ac943ceea57fbe2599963c37f961c
Josh Cox <josh@webhosting.coop>
2014-11-25 15:24:44 -0600

    proxy adder in build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 #/usr/bin/time -v rake build_vagrant_without_tests
+
+cp http/ubuntu-14.04/preseed.cfg.oltorf http/ubuntu-14.04/preseed.cfg
 /usr/bin/time -v rake build_qemu
+cp http/ubuntu-14.04/preseed-noproxy.cfg http/ubuntu-14.04/preseed.cfg
 # beeper
 #aplay /usr/share/sounds/alsa/Front_Center.wav
 ogg123 /usr/share/sounds/KDE-Im-Irc-Event.ogg
